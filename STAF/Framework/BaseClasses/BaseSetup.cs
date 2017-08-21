8f168a80d2f8daad94ad5626c5d71f51b11740ce
Davron Aliyev <davronaliyev@gmail.com>
2017-08-21 16:10:19 -0400

    Extent report upgrade

--- STAF/Framework/BaseClasses/BaseSetup.cs
+++ STAF/Framework/BaseClasses/BaseSetup.cs
@@ -1,23 +1,79 @@
-﻿using NUnit.Framework;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Configuration;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
+using System;
 using System.Configuration;
 
 namespace STAF.Framework.BaseClasses
 {
     class BaseSetup
     {
+        protected ExtentReports _extent;
+        protected ExtentTest _test;
+
+
+
+
+
+
+        [OneTimeSetUp]
+        protected void Setup()
+        {
+            var dir = TestContext.CurrentContext.TestDirectory + ConfigurationManager.AppSettings["ReportPath"];
+            //var now = DateTime.Now.ToString("MM-dd-yyyy H-mm ");
+            //var fileName = this.GetType().ToString() + now + "Test Report.html";
+            var htmlReporter = new ExtentHtmlReporter(dir + DateTime.Now.ToString("MM-dd-yyyy H-mm ") + "Test Report.html");
+
+
+
+            // theme - standard, dark
+            htmlReporter.Configuration().Theme = Theme.Standard;
+
+
+            // report title
+            htmlReporter.Configuration().ReportName = "Automation Report";
+
+
+
+            _extent = new ExtentReports();
+            _extent.AttachReporter(htmlReporter);
+        }
+
+        [OneTimeTearDown]
+        protected void TearDown()
+        {
+            _extent.Flush();
+        }
+
+
+
+
         internal static IWebDriver Instance { get; set; }
         [SetUp]
         public void BaseInitialize()
         {
+            _test = _extent.CreateTest(TestContext.CurrentContext.Test.Name);
+
+
+
             switch (ConfigurationManager.AppSettings["Browser"].ToString())
             {
                 case "Chrome":
                     Instance = new ChromeDriver(GetChromeOptions());
                     Instance.Navigate().GoToUrl(ConfigurationManager.AppSettings["URL"]);
+
+
+                    _test.Log(Status.Pass, "Chrome browser open");
+
+
+
+
                     break;
                 case "Firefox":
                     Instance = new FirefoxDriver();
@@ -31,11 +87,47 @@ namespace STAF.Framework.BaseClasses
                     break;
             }
 
+
+
+
+
+
         }
         [TearDown]
         public void BaseCleanUp()
         {
+
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stacktrace = string.IsNullOrEmpty(TestContext.CurrentContext.Result.StackTrace)
+                    ? ""
+                    : string.Format("{0}", TestContext.CurrentContext.Result.StackTrace);
+            Status logstatus;
+
+            switch (status)
+            {
+                case TestStatus.Failed:
+                    logstatus = Status.Fail;
+                    break;
+                case TestStatus.Inconclusive:
+                    logstatus = Status.Warning;
+                    break;
+                case TestStatus.Skipped:
+                    logstatus = Status.Skip;
+                    break;
+                default:
+                    logstatus = Status.Pass;
+                    break;
+            }
+
+            _test.Log(logstatus, "Test ended with " + logstatus + stacktrace);
+            _extent.Flush();
+
+
             Instance.Quit();
+
+
+
+
         }
         private static InternetExplorerOptions IEOptions
         {
@@ -55,7 +147,7 @@ namespace STAF.Framework.BaseClasses
         {
             ChromeOptions option = new ChromeOptions();
             option.AddArgument("start-maximized");
-            //option.AddArgument("--headless");
+            option.AddArgument("--headless");
             return option;
         }
     }
