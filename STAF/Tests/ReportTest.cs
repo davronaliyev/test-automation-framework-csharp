238552e1fc4f5f2b2b4db2d9323ec8f5c03243a9
Davron Aliyev <davronaliyev@gmail.com>
2017-08-22 16:20:45 -0400

    Test Report screenshot test

--- STAF/Tests/ReportTest.cs
+++ STAF/Tests/ReportTest.cs
@@ -1,5 +1,7 @@
-﻿using NUnit.Framework;
+﻿using AventStack.ExtentReports;
+using NUnit.Framework;
 using STAF.Framework.BaseClasses;
+using STAF.Framework.HelperClasses;
 
 namespace STAF.Tests
 {
@@ -17,7 +19,16 @@ namespace STAF.Tests
         [Test]
         public void ReportTest02()
         {
-            _test.Pass("Test 2");
+            _test.Info("Step 1");
+            _test.Warning("details", MediaEntityBuilder.CreateScreenCaptureFromPath(@"C:\Users\daliyev\OneDrive - DSS\Projects\Git\STA-framework-with-csharp\Reports\screenshots\" + ScreenshotHelper.TakeScreenshot()).Build());
+            _test.Info("Step 2");
+            _test.Warning("details", MediaEntityBuilder.CreateScreenCaptureFromPath(@"C:\Users\daliyev\OneDrive - DSS\Projects\Git\STA-framework-with-csharp\Reports\screenshots\" + ScreenshotHelper.TakeScreenshot()).Build());
+            _test.Info("Step 3");
+            _test.Warning("details", MediaEntityBuilder.CreateScreenCaptureFromPath(@"C:\Users\daliyev\OneDrive - DSS\Projects\Git\STA-framework-with-csharp\Reports\screenshots\" + ScreenshotHelper.TakeScreenshot()).Build());
+
+
+
+
         }
         [Test]
         public void ReportTest03()
