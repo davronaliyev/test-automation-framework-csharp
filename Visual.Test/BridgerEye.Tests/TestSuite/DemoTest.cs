743fb99a2103b334684a96d3f0f97e448a5f6c48
Davron Aliyev <AliyDa01@risk.regn.net>
2019-09-12 14:34:46 -0400

    Adding test project

---
+++ Visual.Test/BridgerEye.Tests/TestSuite/DemoTest.cs
@@ -0,0 +1,34 @@
+﻿using BridgerEye.Framework.Base;
+using NUnit.Framework;
+
+namespace BridgerEye.Tests.TestSuite
+{
+    [TestFixture()]
+    public class DemoTest : TestBase
+    {
+        [TestCase("English")]
+        //[TestCase("Spanish")]
+        //[TestCase("Portugues")]
+        //[TestCase("Deutsch")]
+        //[TestCase("French")]
+        //[TestCase("Chinese")]
+        //[TestCase("Japanese")]
+        public void RealTimeSearch_DemoTestCase(string language)
+        {
+            LoginPage.LinkChecker(19);
+
+            //// Arrange
+            //LoginPage
+            //    .ChangeLanguage(language)
+            //    .Signin();
+
+            //// Act
+            //Search
+            //    .ListScreening("DAVID", "WASHINGTON")
+            //    .HideAllMatches();
+
+            //// Assert
+            //VisualEye.Scan("Results");
+        }
+    }
+}
